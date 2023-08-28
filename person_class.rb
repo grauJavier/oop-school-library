e9ef8ab1aeeb7892bb156a6a41df3584856aabc7
Javier Grau <javiergrau.freelance@gmail.com>
2023-08-28 16:43:55 -0400

    Add classes and methods of: Person, Student & Teacher

--- person_class.rb
+++ person_class.rb
@@ -17,25 +17,3 @@ class Person
     end
   end
 end
-
-class Student < Person
-  def
-    super(_name, _age, _parent_permission)
-    @classroom
-  end
-
-  def play_hooky
-    '¯\(ツ)/¯'
-  end
-end
-
-class Teacher < Person
-  def
-    super(_name, _age)
-    @specialization
-  end
-
-  def can_use_services?
-    true
-  end
-end
---
+++ student_class.rb
@@ -0,0 +1,12 @@
+require_relative 'person_class'
+
+class Student < Person
+  def
+    super(_name, _age, _parent_permission)
+    @classroom
+  end
+
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
+end
