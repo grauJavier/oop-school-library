280df556c24eea958a50bece806bc0b52c0b8583
Javier Grau <javiergrau.freelance@gmail.com>
2023-08-31 17:23:21 -0400

    Fix bug

--- student.rb
+++ student.rb
@@ -5,12 +5,16 @@ class Student < Person
 
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
-    assign_to_classroom(classroom) if classroom
+    assign_classroom(classroom) if classroom
   end
 
   def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
+  end
+
+  def assign_classroom(classroom)
+    self.classroom = classroom
   end
 
   def play_hooky
